e817bc6014b0e37e7b390619cdfa29f440f32f09
nickbenve <nbenvenaste@frba.utn.edu.ar>
2022-05-13 22:55:27 -0300

    agrego validation

--- inscripcionMaterias/src/main/java/inscripcionMaterias/Materia.java
+++ inscripcionMaterias/src/main/java/inscripcionMaterias/Materia.java
@@ -3,8 +3,16 @@ package inscripcionMaterias;
 import java.util.ArrayList;
 import java.util.Collection;
 
+
+import javax.validation.constraints.NotBlank;
+
 public class Materia {
+	
+	@NotBlank
 	private String nombre;
+	
+//	@Min(2000)
+//	private Integer anio;
 	Collection<Materia> correlativas;
 	
 	public Materia(String nombre, Collection<Materia> materiasExiguidas) {
